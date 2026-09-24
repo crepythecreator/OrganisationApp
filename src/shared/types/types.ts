f11887876ca9b87d2f67d1881e608eea4cb33b83
Misha Zhuravlev <mishami816@gmail.com>
2026-09-24 12:39:14 +0300

    add: statusUI component

---
+++ src/shared/types/types.ts
@@ -0,0 +1,9 @@
+export type statusTypes = "Published" | "Draft" | "Review" | "SoldOut" | "Finished";
+
+export const statusWords = {
+  "Published": "Опубликовано",
+  "Draft": "Черновик",
+  "Review": "На проверке",
+  "SoldOut": "Аншлаг",
+  "Finished": "Завершено"
+}
